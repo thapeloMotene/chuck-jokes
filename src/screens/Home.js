88b2f9eefd062c499c44726c10653d9afe026e87
Thapelo Motene <thapelo.motene@gmail.com>
2019-12-12 17:30:03 +0200

    class to className

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -10,12 +10,12 @@ export default class Home extends Component {
   
   <div className="main-container">
     <h3 className="text-center">View Chuck jokes by category</h3>
-    <p class="p2 text-center" >Click a category tile to view a radom chuck norris joke in that category</p>
+    <p className="p2 text-center" >Click a category tile to view a radom chuck norris joke in that category</p>
   
-  <div class="clr-row clr-justify-content-center">
+  <div className="clr-row clr-justify-content-center">
     
-      <div class="clr-col-10">
-      <div class="clr-row clr-justify-content-center">
+      <div className="clr-col-10">
+      <div className="clr-row clr-justify-content-center">
       <CategoryList id="category-list"/>
       </div>
   
